b247333d70c8e1d5ac26dcaa4dbd24e4ef5b90f0
Mehulcoder <mehul355180@gmail.com>
2020-04-13 21:04:44 +0530

    Commented

--- Coding-Question/perms.js
+++ Coding-Question/perms.js
@@ -10,28 +10,37 @@ let perms = (string) => {
     //In case of wrong input or empty input
     if (!string || typeof(string) !== "string"){
       return "Please enter a valid string"
-    } else if (string.length < 2 ){
+    } else if (string.length <= 1 ){
       return string
     }
   
     //Result array
     let result = [];
-     
-    //Assume you have disassembled all the characters and are 
-    //Pick a character from the box
-    for (let i = 0; i < string.length; i++){
-      let char = string[i]
-  
-      if (string.indexOf(char) != i){
-        continue;
-      } 
-  
-      let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
-  
-      for (let permutation of perms(remainingChars)){
-        result.push(char + permutation) }
-    }
-    return result
-  }
 
-console.log(perms("AABC"));
+
+    //Pick a character from the string one by one
+    for (let i = 0; i < string.length; i++){
+
+        //Picked a character starting from the left
+        let char = string[i]
+        
+        //If the same character has not been picked before : continue
+        if (string.indexOf(char) != i){
+            continue;
+        } 
+        
+        //Remaining characters other than the picked one
+        let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length)
+        
+        //Put all perms of remaining chars in the result array
+        for (let permutation of perms(remainingChars)){
+            //Concate the chosen character in each perm in the array.
+            result.push(char + permutation) 
+        }
+    }
+    return result;
+}
+
+var input = "aabc"
+
+console.log(perms(input), perms(input).length);
